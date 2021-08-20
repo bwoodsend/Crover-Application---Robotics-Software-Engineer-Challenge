98a96eec09267e2e1dfe49fc954af282ced2aeb8
bwoodsend <bwoodsend@gmail.com>
2021-08-20 10:34:29 +0100

    Add more GNSS graphs.

--- writeup/gnss.rst
+++ writeup/gnss.rst
@@ -39,6 +39,27 @@ This is because a side effect of these long-rolling averages is an inertia that
 makes the estimated path appear to cut or at least be lethargic whilst taking
 corners.
 
+In other words, the window sizes for the rolling average function is a trade-off
+between the position being upto-date, and smoothing out the noise due to error
+from the positional sensor.
+The chart below measures the mean absolute position error for different window sizes, showing
+a U-shaped curve with a minimum at n=5 giving an error of 0.458.
+
+.. plot::
+
+    from gnss_plots import show_smooth_path_error
+    show_smooth_path_error()
+
+An advantage of GNSS positional information is that it does not drift over time, which might cause
+positional errors to accumulate. The plot below (for the best window size n=5) shows that
+although the error in position is noisy, the errors do not increase over time.
+
+.. plot::
+
+    from gnss_plots import show_smooth_path_error_over_time
+    show_smooth_path_error_over_time(5)
+
+
 In summary, a short rolling average of GNSS readings can be used to track the
 car's long-term position but for short maneuvers, it would be less suitable.
 
--- writeup/gnss_plots.py
+++ writeup/gnss_plots.py
@@ -6,28 +6,39 @@ from crover_challenge.core import interpolate_points, reduce
 from analysis import *
 
 
+def smoothened_path_error(n):
+    """
+    Calculate the mean absolute hypotenuse error of a simple rolling average smoothing on
+    GNSS data.
+    :param n: number of points to include in the average
+    :return: mean square root error against ground truth,
+             smoothed path
+    """
+    mean = RollingMean(n)
+
+    # Stream the values into the rolling mean. Record the averages.
+    smooth_path = np.empty((len(gnss), 2))
+    for (i, xy) in enumerate(gnss[:, [1, 2]]):
+        smooth_path[i] = mean.new_value(xy)
+
+    error = position_error(smooth_path, gnss[:, 0])
+    return error, smooth_path
+
+
 def show_smoothened(n):
     """Calculate, plot and evaluate the effect of rolling average smoothing on
     GNSS data.
     """
-    mean = RollingMean(n)
-
-    # Stream the values into the rolling mean. Log the means.
-    points = np.empty((len(gnss), 2))
-    for (i, xy) in enumerate(gnss[:, [1, 2]]):
-        points[i] = mean.new_value(xy)
-
-    error = position_error(points, gnss[:, 0])
-
+    error, smooth_path = smoothened_path_error(n)
     if n:
         pylab.title(
             f'GNSS position, "last {n}" rolling average. '
             f'Mean error: {error:.3f}')
-        path(points, color="r", label="GNSS")
+        path(smooth_path, color="r", label="GNSS")
     else:
         pylab.title(f"Raw GNSS position vs real position. "
                     f"Mean error: {error:.3f}")
-        path(points, color="r", label="GNSS")
+        path(smooth_path, color="r", label="GNSS")
     path(ground_truth[:, [1, 2]], label="Ground Truth", color="c")
 
     pylab.legend()
@@ -35,3 +46,39 @@ def show_smoothened(n):
     pylab.ylabel("$y$")
 
     pylab.show()
+
+
+def show_smooth_path_error():
+    """ Show a chart of average error wrt window size """
+    MAX_N = 50
+    path_error_table = np.empty((MAX_N, 2))
+    for n in range(MAX_N):
+        error, _ = smoothened_path_error(n)
+        path_error_table[n] = (n, error)
+    pylab.title("Error wrt rolling average window size")
+    pylab.plot(path_error_table[:, 0], path_error_table[:, 1])
+    pylab.ylabel("Mean absolute error")
+    pylab.xlabel("Window size $n$")
+    pylab.show()
+
+
+def show_smooth_path_error_over_time(n):
+    """ Show a chart of average error of a path over time """
+    _, smooth_path = smoothened_path_error(n)
+    # T = number of time points
+    T = len(smooth_path)
+    path_error_table = np.empty((T, 1))
+    for t in range(1, T):
+        error = position_error(smooth_path[:t], gnss[:t, 0])
+        path_error_table[t] = error
+    pylab.title("Positional error over time, $n=5$")
+    pylab.plot(path_error_table)
+    pylab.ylabel("Mean absolute positional error")
+    pylab.xlabel("Time point")
+    pylab.show()
+
+
+"""
+from gnss_plots import show_smooth_path_error_over_time
+show_smooth_path_error_over_time(5)
+"""
